21e0dac95a8e422c0a2e7accdaf1c6f4306a31bf
Roni <ronisilber@gmail.com>
2020-07-01 14:53:25 +0300

    LoginActivity

--- app/src/main/java/com/instafood/LoginActivity.java
+++ app/src/main/java/com/instafood/LoginActivity.java
@@ -1,14 +1,43 @@
 package com.instafood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnSign, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginFragment loginFrag = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.Loginfragment);
+
+        SignupFragment signFrag = new SignupFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.Login_Container, signFrag, "TAG");
+        transaction.commit();
+
+
+        //init();
+
+       //btnSign.setOnClickListener(new View.OnClickListener() {
+       //    @override
+
+       //    )};
+
     }
+
+    private void init()
+    {
+        //btnLogin = findViewById(R.id.btnLogin);
+        //btnSign = findViewById((R.id.btnSign);
+    }
+
 }
